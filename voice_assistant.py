1bbe0d12779b21f24d2f07329b1be59febff5617
Brandon Isahir Hernández Camacho <isahernz@outlook.com>
2024-04-11 17:32:56 -0600

    Update voice assistant conversation logic to handle multiple interactions and improve user experience

--- voice_assistant.py
+++ voice_assistant.py
@@ -4,13 +4,27 @@ import os
 import speech_recognition as speech_r
 import webbrowser
 
-r = speech_r.Recognizer() 
-mic  = speech_r.Microphone()  
+
+# Dicctionaries of words
+synonymsThanks = ["GRACIAS", "NADA", "ES TODO", "ADIÓS", "BYE"]
+synonymsMusic = ["MÚSICA", "CANCIONES", "ROLA", "ROLITA"]
+synonymsRock = ["ROCK"]
+synonymsLove = ["BALADA", "ROMÁNTICA", "AMOR"]
+synonymsSonidero = ["SONIDERA", "BAILE", "CUMBIA"]
+
+def searchWordInDicctionary(dicctionaryWordsContext, sentence):
+    words = sentence.split()
+    for word in words:
+      if word in dicctionaryWordsContext:
+        return True
+    return False
 
 def createVoiceDialog(text):
   try:
     text_voice = gTTS(text = text, lang='es-us')
-    text_voice.save("archivo.mp3")
+    x = open("archivo.mp3","wb+")
+    text_voice.write_to_fp(x)
+    x.close()
     playsound("archivo.mp3")
   except Exception as e:
     print("Error: ", e)
@@ -18,31 +32,75 @@ def createVoiceDialog(text):
     os.remove("archivo.mp3")
 
 def recognizeAudio():
+  r = speech_r.Recognizer()
+  mic  = speech_r.Microphone()
   with mic as dialog:
           try:
             print("Escuchando")
             audio = r.listen(dialog)
-          except LookupError: 
-            print ("No entiendo")
+          except TypeError as e: 
+            print ("No entiendo", e)
   transcript = r.recognize_google(audio, language="es-MX")
   transcript = transcript.upper()
-  print('el audio dice {}'.format(transcript))
+  print(f"El audio dice {transcript.format()}")
   return transcript
 
+def askIfExit():
+  createVoiceDialog("¿Te puedo ayudar en algo más")
+  transcript = recognizeAudio()
+  if searchWordInDicctionary(synonymsThanks, transcript):
+    return True
+  else:
+    createVoiceDialog("¿Qué puedo hacer por ti?")
+
 def createConversation():
-    createVoiceDialog("Hola, ¿en qué puedo ayudarte?")
+    createVoiceDialog("Hola, soy tu asistente virtual, ¿en qué puedo ayudarte?")
     transcript = recognizeAudio()
-    while "GRACIAS" not in transcript:
-      if "PELÍCULAS" in transcript:
-        createVoiceDialog("Puedes ir al cine o ver una película en casa")
-        transcript = recognizeAudio()
-        if "ABRE" in transcript:
-            url = "https://www.youtube.com/watch?v=jzfJU-J9UL8"
+    while True:
+      if searchWordInDicctionary(synonymsThanks, transcript):
+        createVoiceDialog("¡Fue un placer ayudarte!")
+        return False
+
+      # "Quiero escuchar música, cancion, melodia, sonido"
+      if searchWordInDicctionary(synonymsMusic, transcript):
+          createVoiceDialog("¿Qué tipo de música te gustaría escuchar?")
+          transcript = recognizeAudio()
+
+          if searchWordInDicctionary(synonymsRock, transcript):
+              createVoiceDialog("¡Aquí tienes tu canción!")
+              url = "https://www.youtube.com/watch?v=HQMdiGXYtCM"
+              webbrowser.get().open(url)
+              if askIfExit():
+                createVoiceDialog("¡Fue un placer ayudarte!")
+                break
+              else:
+                transcript = recognizeAudio()
+                continue
+      
+          elif searchWordInDicctionary(synonymsLove, transcript):
+              createVoiceDialog("¡Aquí tienes tu canción!")
+              url = "https://www.youtube.com/watch?v=ipICdGojxug&t=1s"
+              webbrowser.get().open(url)
+              if askIfExit():
+                createVoiceDialog("¡Fue un placer ayudarte!")
+                break
+              else:
+                transcript = recognizeAudio()
+                continue
+
+          elif searchWordInDicctionary(synonymsSonidero, transcript):
+            createVoiceDialog("¡Aquí tienes tu canción!")
+            url = "https://www.youtube.com/watch?v=aGpEBxazyq4"
             webbrowser.get().open(url)
-            createVoiceDialog("¿Hay algo más en lo que pueda ayudarte?")
-            transcript = recognizeAudio()
+            if askIfExit():
+              createVoiceDialog("¡Fue un placer ayudarte!")
+              break
+            else:
+              transcript = recognizeAudio()
+              continue
+
       else:
           createVoiceDialog("No entiendo, ¿puedes repetir?")
           transcript = recognizeAudio()
-    createVoiceDialog("Fue un placer ayudarte")
+
 createConversation()
